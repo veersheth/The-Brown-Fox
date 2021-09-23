ef52d10b34cff7a7f42f161bf3c91f239d389235
Veer <vtube258@gmail.com>
2021-09-23 09:06:52 +0530

    Compares input with actual text

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Works");
+const referenceText = document.getElementById("referenceText")
+const inputVal = document.getElementById("inputVal")
+
+var totalIncorrect = 0
+
+inputVal.addEventListener('input', () => {
+    const letters_array = referenceText.querySelectorAll('span')
+    const arrayVal = inputVal.value.split('')
+    letters_array.forEach((characterSpan, index) => {
+        const character = arrayVal[index]
+        if (character == null) 
+            // IF IT DOESN'T EXIST
+            {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+        } else if (character === characterSpan.innerText) 
+            // IF IT IS CORRECT
+            {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else 
+            // IF IT IS WRONG
+             {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            totalIncorrect = totalIncorrect + 1
+        }
+    })
+})
+
